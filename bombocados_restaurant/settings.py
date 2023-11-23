76dfde0142fb575d6dcf81f569b612cf89f390a4
douglas curcio de melo <douglasengmelo@gmail.com>
2023-11-23 11:26:20 +0000

    Enhance site layout and configuration

--- bombocados_restaurant/settings.py
+++ bombocados_restaurant/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,7 +58,7 @@ TEMPLATE_DIR = Path(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
